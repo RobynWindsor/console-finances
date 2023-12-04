54d2402613b6054765abf440099bfbb4d95d6107
Robyn <127926809+Robyn-web@users.noreply.github.com>
2023-12-04 13:40:41 +0000

    Added if statements to compare monthlyChange variable to find largest increase and decereases in array

--- index.js
+++ index.js
@@ -1,94 +1,94 @@
 var finances = [
-  ["Jan-2010", 867884],
-  ["Feb-2010", 984655],
-  ["Mar-2010", 322013],
-  ["Apr-2010", -69417],
-  ["May-2010", 310503],
-  ["Jun-2010", 522857],
-  ["Jul-2010", 1033096],
-  ["Aug-2010", 604885],
-  ["Sep-2010", -216386],
-  ["Oct-2010", 477532],
-  ["Nov-2010", 893810],
-  ["Dec-2010", -80353],
-  ["Jan-2011", 779806],
-  ["Feb-2011", -335203],
-  ["Mar-2011", 697845],
-  ["Apr-2011", 793163],
-  ["May-2011", 485070],
-  ["Jun-2011", 584122],
-  ["Jul-2011", 62729],
-  ["Aug-2011", 668179],
-  ["Sep-2011", 899906],
-  ["Oct-2011", 834719],
-  ["Nov-2011", 132003],
-  ["Dec-2011", 309978],
-  ["Jan-2012", -755566],
-  ["Feb-2012", 1170593],
-  ["Mar-2012", 252788],
-  ["Apr-2012", 1151518],
-  ["May-2012", 817256],
-  ["Jun-2012", 570757],
-  ["Jul-2012", 506702],
-  ["Aug-2012", -1022534],
-  ["Sep-2012", 475062],
-  ["Oct-2012", 779976],
-  ["Nov-2012", 144175],
-  ["Dec-2012", 542494],
-  ["Jan-2013", 359333],
-  ["Feb-2013", 321469],
-  ["Mar-2013", 67780],
-  ["Apr-2013", 471435],
-  ["May-2013", 565603],
-  ["Jun-2013", 872480],
-  ["Jul-2013", 789480],
-  ["Aug-2013", 999942],
-  ["Sep-2013", -1196225],
-  ["Oct-2013", 268997],
-  ["Nov-2013", -687986],
-  ["Dec-2013", 1150461],
-  ["Jan-2014", 682458],
-  ["Feb-2014", 617856],
-  ["Mar-2014", 824098],
-  ["Apr-2014", 581943],
-  ["May-2014", 132864],
-  ["Jun-2014", 448062],
-  ["Jul-2014", 689161],
-  ["Aug-2014", 800701],
-  ["Sep-2014", 1166643],
-  ["Oct-2014", 947333],
-  ["Nov-2014", 578668],
-  ["Dec-2014", 988505],
-  ["Jan-2015", 1139715],
-  ["Feb-2015", 1029471],
-  ["Mar-2015", 687533],
-  ["Apr-2015", -524626],
-  ["May-2015", 158620],
-  ["Jun-2015", 87795],
-  ["Jul-2015", 423389],
-  ["Aug-2015", 840723],
-  ["Sep-2015", 568529],
-  ["Oct-2015", 332067],
-  ["Nov-2015", 989499],
-  ["Dec-2015", 778237],
-  ["Jan-2016", 650000],
-  ["Feb-2016", -1100387],
-  ["Mar-2016", -174946],
-  ["Apr-2016", 757143],
-  ["May-2016", 445709],
-  ["Jun-2016", 712961],
-  ["Jul-2016", -1163797],
-  ["Aug-2016", 569899],
-  ["Sep-2016", 768450],
-  ["Oct-2016", 102685],
-  ["Nov-2016", 795914],
-  ["Dec-2016", 60988],
-  ["Jan-2017", 138230],
-  ["Feb-2017", 671099],
+  ['Jan-2010', 867884],
+  ['Feb-2010', 984655],
+  ['Mar-2010', 322013],
+  ['Apr-2010', -69417],
+  ['May-2010', 310503],
+  ['Jun-2010', 522857],
+  ['Jul-2010', 1033096],
+  ['Aug-2010', 604885],
+  ['Sep-2010', -216386],
+  ['Oct-2010', 477532],
+  ['Nov-2010', 893810],
+  ['Dec-2010', -80353],
+  ['Jan-2011', 779806],
+  ['Feb-2011', -335203],
+  ['Mar-2011', 697845],
+  ['Apr-2011', 793163],
+  ['May-2011', 485070],
+  ['Jun-2011', 584122],
+  ['Jul-2011', 62729],
+  ['Aug-2011', 668179],
+  ['Sep-2011', 899906],
+  ['Oct-2011', 834719],
+  ['Nov-2011', 132003],
+  ['Dec-2011', 309978],
+  ['Jan-2012', -755566],
+  ['Feb-2012', 1170593],
+  ['Mar-2012', 252788],
+  ['Apr-2012', 1151518],
+  ['May-2012', 817256],
+  ['Jun-2012', 570757],
+  ['Jul-2012', 506702],
+  ['Aug-2012', -1022534],
+  ['Sep-2012', 475062],
+  ['Oct-2012', 779976],
+  ['Nov-2012', 144175],
+  ['Dec-2012', 542494],
+  ['Jan-2013', 359333],
+  ['Feb-2013', 321469],
+  ['Mar-2013', 67780],
+  ['Apr-2013', 471435],
+  ['May-2013', 565603],
+  ['Jun-2013', 872480],
+  ['Jul-2013', 789480],
+  ['Aug-2013', 999942],
+  ['Sep-2013', -1196225],
+  ['Oct-2013', 268997],
+  ['Nov-2013', -687986],
+  ['Dec-2013', 1150461],
+  ['Jan-2014', 682458],
+  ['Feb-2014', 617856],
+  ['Mar-2014', 824098],
+  ['Apr-2014', 581943],
+  ['May-2014', 132864],
+  ['Jun-2014', 448062],
+  ['Jul-2014', 689161],
+  ['Aug-2014', 800701],
+  ['Sep-2014', 1166643],
+  ['Oct-2014', 947333],
+  ['Nov-2014', 578668],
+  ['Dec-2014', 988505],
+  ['Jan-2015', 1139715],
+  ['Feb-2015', 1029471],
+  ['Mar-2015', 687533],
+  ['Apr-2015', -524626],
+  ['May-2015', 158620],
+  ['Jun-2015', 87795],
+  ['Jul-2015', 423389],
+  ['Aug-2015', 840723],
+  ['Sep-2015', 568529],
+  ['Oct-2015', 332067],
+  ['Nov-2015', 989499],
+  ['Dec-2015', 778237],
+  ['Jan-2016', 650000],
+  ['Feb-2016', -1100387],
+  ['Mar-2016', -174946],
+  ['Apr-2016', 757143],
+  ['May-2016', 445709],
+  ['Jun-2016', 712961],
+  ['Jul-2016', -1163797],
+  ['Aug-2016', 569899],
+  ['Sep-2016', 768450],
+  ['Oct-2016', 102685],
+  ['Nov-2016', 795914],
+  ['Dec-2016', 60988],
+  ['Jan-2017', 138230],
+  ['Feb-2017', 671099],
 ];
 
 // Calculate total number of months included in the dataset
-console.log("Total Months:" + finances.length);
+console.log('Total Months:' + finances.length);
 
 // Created Loop to add values of [1] index of array
 // Claculate the net total of amount of profit/losses over the entire period
@@ -96,7 +96,66 @@ var netTotalProfitLosses = 0;
 for (var i = 0; i < finances.length; i++) {
   netTotalProfitLosses = netTotalProfitLosses + finances[i][1];
 }
-console.log("Total Profits/Losses £:" + netTotalProfitLosses);
+console.log('Total Profits/Losses £:' + netTotalProfitLosses);
+
+// CALCULATE AVERAGE CHANGES
+var totalChange = 0;
+var maxChange = 0;
+var minChange = 0;
+for (let i = 0; i < finances.length; i++) {
+  var currentMonth = finances[i][1];
+  if (finances[i + 1]) {
+    var nextMonth = finances[i + 1][1];
+    var monthlyChange = nextMonth - currentMonth;
+    totalChange = totalChange + monthlyChange;
+    if (monthlyChange > maxChange) {
+      maxChange = monthlyChange;
+      maxChangeMonth = finances[i + 1][0];
+    }
+    if (monthlyChange < minChange) {
+      minChange = monthlyChange;
+      minChangeMonth = finances[i + 1][0];
+    }
+  }
+}
+
+const average = totalChange / (finances.length - 1);
+console.log('Average Change:' + Math.round(average * 100) / 100);
+console.log(
+  `Greatest Increase in Profits/Losses: ${maxChangeMonth} £ ${maxChange}`
+);
+console.log(
+  `Greatest Decrease in Profits/Losses: ${minChangeMonth} £ ${minChange}`
+);
+
+// CLACULATE GREATEST INCREASE IN CHANGES
+
+// var finances = [
+//   ['Jan-2010', 867884],
+//   ['Feb-2010', 984655],
+//   ['Mar-2010', 322013],
+//   ['Apr-2010', -69417],
+//   ['May-2010', 310503],
+// ];
+
+// const maxDiff = 0;
+// for (let i = 0; i < finances.length; i++) {
+//   const current = finances[i][1];
+//   if (finances[i + 1]) {
+//     const next = finances[i + 1][1];
+//     const monthDiff = next - currentMonth;
+//   }
+// } // calculate difference between current month and next month then store that differenc
+
+//
+// You will need to track what the total change in profit/losses
+
+// The total number of months included in the dataset.
+
+// You will need to track what the total change in Profit/Losses are from month to month and then find the
+// (Total/number of months -1)
+// The greatest increase in profit/losses (date and amount) over the entire period
+// the greatest decrease in profit/losses (date and amount) over the entire period.
 
 // var totalChanges = 0;
 // var AverageOfChanges = 0;
@@ -116,40 +175,11 @@ console.log("Total Profits/Losses £:" + netTotalProfitLosses);
 // ["Mar-2010", 322013],
 // ["Apr-2010", -69417],
 // ["May-2010", 310503],
+// ["Jun-2010", 522857],
+// ["Jul-2010", 1033096],
+// ["Aug-2010", 604885],
+// ["Sep-2010", -216386],
+// ["Oct-2010", 477532],
 
 // const jan = finances[0][1];
 // const feb = finances[1][1];
-
-var totalChange = 0;
-
-for (let i = 0; i < finances.length; i++) {
-  var currentMonth = finances[i][1];
-
-  // if (!finances[i + 1]) return; - this gives an error stating an illegel retun was made and that is because the code has nowehere to return to. It would need to be in a function
-
-  if (finances[i + 1]) {
-    var nextMonth = finances[i + 1][1];
-    var monthlyChange = nextMonth - currentMonth;
-    totalChange = totalChange + monthlyChange;
-  }
-}
-
-console.log(totalChange);
-const average = totalChange / (finances.length - 1);
-console.log(average);
-
-// console.log("finances: ", finances);
-// console.log("finances[i]: ", finances[0]);
-// console.log("finances[i][1]: ", finances[0][1]);
-
-// console.log(averageChanges);
-// will need to calculate the changes between each value and then divide them by 86
-
-// You will need to track what the total change in profit/losses
-
-// The total number of months included in the dataset.
-
-// You will need to track what the total change in Profit/Losses are from month to month and then find the
-// (Total/number of months -1)
-// The greatest increase in profit/losses (date and amount) over the entire period
-// the greatest decrease in profit/losses (date and amount) over the entire period.
